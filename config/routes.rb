9907658af69a85b440f1a33b894c17cc4fa88152
Arthur HUANG <ahuang@xogrp.com>
2014-09-26 15:55:02 +0800

    db

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Demo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root :to => 'welcome#index'
-  root 'welcome#index'
+  root 'people#index'
+
+  resources :people
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
